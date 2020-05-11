f2155bb9919b54a97b9d191760b74667beb20034
David Childers <dchilders21@gmail.com>
2020-05-11 10:28:00 -0700

    Round 2 updates

--- mail_handler.php
+++ mail_handler.php
@@ -7,8 +7,8 @@ if(isset($_POST['submit'])){
     $subject = "Form submission";
     $message = $first_name . " " . $last_name . " (" . $_POST['email'] . ") wrote the following:" . "\n\n" . $_POST['message'];
     $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.<br />";
+    echo "Return to <a href=\"http://www.zoommultisports.com\">Homepage</a>";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
